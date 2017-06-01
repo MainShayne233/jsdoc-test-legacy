8661ebff56986341f6a42a35e27b05597d9dbe4f
MainShayne233 <shaynetremblay@hotmail.com>
2017-06-01 11:00:12 -0400

    return value parsing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-export {default as Counter} from './components/counter_container'
+function exampleLineNumbers(file) {
+  return file.split("\n")
+             .map((line, index) => [line, index])
+             .filter(lineAndIndex => lineAndIndex[0].indexOf('@example') !== -1)
+             .map(lineAndIndex => lineAndIndex[1])
+}
+
+function expectedReturnValue(file, exampleIndex) {
+  const string = returnValueString(file, exampleIndex).split('* //=>')[1]
+  return eval(`( ${string} )`)
+}
+
+function returnValueString(file, exampleIndex) {
+  while(true) {
+    const line = file.split("\n")[exampleIndex]
+    if (line === undefined) throw(new Error('could not find expected return for example (expected return denoated by //=>)'))
+    else if(line.indexOf('//=>') !== -1) return line
+    else exampleIndex++
+  }
+}
+
+module.exports = {
+  exampleLineNumbers,
+  expectedReturnValue,
+}
--- test/doctest_test.js
+++ test/doctest_test.js
@@ -1,7 +1,44 @@
 import { expect } from 'chai'
+import fs from 'fs'
+import doctest from '../src'
+
+const stringModuleFile = fs.readFileSync('test/support/example_modules/string.js', 'utf8')
 
 describe('Doctest', () => {
-  it ('works', () => {
-    expect(true).to.equal(true)
+
+  describe('exampleLineNumbers/1', () => {
+    it('returns a list of line numbers that have @example in them', () => {
+      const exampleLineNumbers = doctest.exampleLineNumbers(stringModuleFile)
+      expect( exampleLineNumbers.length ).to.equal(4)
+      expect( exampleLineNumbers[0] ).to.equal(6)
+      expect( exampleLineNumbers[1] ).to.equal(9)
+      expect( exampleLineNumbers[2] ).to.equal(26)
+      expect( exampleLineNumbers[3] ).to.equal(44)
+    })
+  })
+
+  describe('expectedReturnValue/2', () => {
+    it('should return the expected return value for the when the function returns a string', () => {
+      const expectedReturnValue = doctest.expectedReturnValue(stringModuleFile, 6)
+      expect( expectedReturnValue ).to.equal('Woah')
+    })
+
+    it('should return the expected return value for the when the function returns an object', () => {
+      const expectedReturnValue = doctest.expectedReturnValue(stringModuleFile, 26)
+      expect( expectedReturnValue.constructor ).to.equal(Object)
+      expect( expectedReturnValue.length ).to.equal(4)
+      expect( expectedReturnValue.vowels ).to.equal(2)
+      expect( expectedReturnValue.consonants ).to.equal(2)
+    })
+
+    it('should return the expected return value for the when the function returns an array', () => {
+      const expectedReturnValue = doctest.expectedReturnValue(stringModuleFile, 44)
+      expect( expectedReturnValue.constructor ).to.equal(Array)
+      expect( expectedReturnValue[0] ).to.equal('why')
+      expect( expectedReturnValue[1] ).to.equal('am')
+      expect( expectedReturnValue[2] ).to.equal('i')
+      expect( expectedReturnValue[3] ).to.equal('doing')
+      expect( expectedReturnValue[4] ).to.equal('this?')
+    })
   })
 })
